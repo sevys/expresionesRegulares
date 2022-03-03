a19d86ec5c298f4d8784795767db282911c6b119
Tu Nombre <you@example.com>
2022-03-02 22:55:21 -0600

    implementando hilos

--- projectoExpresion.py
+++ projectoExpresion.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import re
 import os
+import threading
+import time
 
 #192.168.1.69
 
@@ -9,11 +11,13 @@ def ip():
     cadenaEntrante = entrada.get()
 
     if cadenaEntrante[1] == '0': 
-        a()
+        threading.Thread(target=a).start()
     elif cadenaEntrante[1] == '7':
-        b()
+        threading.Thread(target=b).start()
     elif cadenaEntrante[1] == '9':
-        c()
+        threading.Thread(target=c).start()
+    else: 
+        salida.set('Lo que ingreso no es una IP')
 
 
 def a():
@@ -21,14 +25,17 @@ def a():
      result = re.fullmatch(candenaA, entrada.get())
 
      if result == None:
+        salida.set('su IP no corresponde con el rango')
         print('Su IP no corresponde con el rango')
      else:
-        print('''
-        ---------------------------------------------------------
-                        SU IP CORRESPONDE
-                    CLASE: A
-        ---------------------------------------------------------''')
-        os.system(f'ping {entrada.get()}')
+        salida.set('''
+    ---------------------------------------------------------
+            SU IP CORRESPONDE A LA
+                CLASE: A
+    ---------------------------------------------------------''')
+        if entrada.get() == '192.168.1.69':
+            os.system(f'ping {entrada.get()}')
+        # os.system(f'ping {entrada.get()})
 
 
 
@@ -37,16 +44,16 @@ def b():
     result = re.fullmatch(cadenaB, entrada.get())
 
     if result == None:
-        print('Su IP no corresponde con el rango')
+        salida.set('su IP no corresponde con el rango')
     else:
-        print('''
-        ---------------------------------------------------------
-                        SU IP CORRESPONDE
-                    CLASE: A
-        ---------------------------------------------------------
-
-        ''')
-        os.system(f'ping {entrada.get()}')
+        salida.set('''
+    ---------------------------------------------------------
+            SU IP CORRESPONDE A LA
+                CLASE: B
+    ---------------------------------------------------------''')
+        if entrada.get() == '192.168.1.69':
+            os.system(f'ping {entrada.get()}')
+        #os.system(f'ping {entrada.get()}')
 
 
 def c():
@@ -54,20 +61,25 @@ def c():
     result = re.fullmatch(cadenaC, entrada.get())
 
     if result == None:
-        print('Su IP no corresponde con el rango')
+        salida.set('su IP no corresponde con el rango')
     else:
-        print('''
-        ---------------------------------------------------------
-                        SU IP CORRESPONDE
-                    CLASE: A
-        ---------------------------------------------------------
+        salida.set('''
+    ---------------------------------------------------------
+            SU IP CORRESPONDE A LA
+                CLASE: C
+    ---------------------------------------------------------''')
+        if entrada.get() == '192.168.1.69':
+            os.system(f'ping {entrada.get()}')
+            time.sleep(10)
+            os.system('^C')
+            
 
-        ''')
-        os.system(f'ping {entrada.get()}')
+        # os.system(f'netstat {entrada.get()}')
 
 
 ventana = Tk()
-ventana.geometry('400x300')
+ventana.geometry('580x440')
+ventana.config(bg='#0E476C')
 
 
 #declaramos variables para obtener los datos 
@@ -77,21 +89,27 @@ salida = StringVar()
 
 
 #declaramos un label para poner halgo
-labe1 = Label(ventana, text='Valida tu IP')
-labe1.place(x= 110, y= 20)
-labelResultado = Label(ventana, textvariable=salida)
-labelResultado.place(x = 100, y = 160)
+title = Label(ventana,text="VALIDAR IP")
+title.place(x=10,y=10, width=560, height=60)
+title.config(font="Ubuntu 30 normal", fg='white', bg='#0E476C')
+
+#campo donse se muestra los resultados
+labelResultado = Label(ventana, textvariable=salida, bg='#FFC12C')
+labelResultado.place(x=130, y=300) 
+
 
 
 #declaramos el cuadro de texto
-cuadro = Entry(ventana, textvariable=entrada)
-cuadro.place(x= 110, y= 40)
+cuadro = Entry(ventana,font='arial 20',justify='center', textvariable=entrada)
+cuadro.place(x=110,y= 130,width=350, height=50)
 
 
 #declaramos el boton
-boton = Button(ventana, text='calcular', command=ip)
-boton.place(x=160, y=80)
+border_color = Frame(ventana, background="#003C6D")
 
+boton = Button(ventana, text='calcular', command=ip)
+boton.place(x=137, y=200, width=300,height=40)
+boton.config(font="Ubuntu 15 normal",bg="#17AEBF",fg='white' )
 
 
 def main():
@@ -100,5 +118,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-#las devdependencias, se utilizan cuando se esta desarollando el proyecto, 
-#las dependence: se utilizan para que el proyecto funcione
+
